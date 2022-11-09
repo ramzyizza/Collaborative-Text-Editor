d6b14194f89a672bf0f547b9b9e6df3dbfafa46f
yviler <91642043+yviler@users.noreply.github.com>
2022-11-09 14:20:30 +0700

    Create server.js

---
+++ server.js
@@ -0,0 +1,10 @@
+const io = require('socket.io')(3001, {     //sets 3001 to make it a different url 
+    cors:   {                               //course origin request support to make the request from a diff url
+        origin: 'htpps://localhost:3000',   //uses the client port
+        methods: ['GET', 'POST'],           
+    },
+})
+
+io.on("connection", () => {
+    console.log("connected")
+})
