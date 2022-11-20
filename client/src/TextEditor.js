a76f24e2fe093fb6a4cb5276e40fc5f91a10b78c
ramzy <ramzy.izza.wardhana@mail.ugm.ac.id>
2022-11-20 12:03:44 +0700

    Added Delta API, Connect socket server n Clients.

---
+++ client/src/TextEditor.js
@@ -0,0 +1,98 @@
+import { useCallback, useEffect, useState} from 'react'
+import React from 'react'
+import Quill from "quill"
+import "quill/dist/quill.snow.css"
+import {io} from 'socket.io-client'
+// import ReactQuill from "react-quill";
+// import EditorToolbar, { modules, formats } from "./EditorToolbar";
+import "./styles.css";
+
+
+//Quill Toolbar Configurations
+const TOOLBAR =  [
+    [{ header: [1,2,3,4,5,6,false]}], //Types of Heading Sizes
+    [{font : []}],
+    [{list: "ordered"}, {list: "bullet"}],
+    ["bold", "italic" , "underline"],
+    [{color: []} , {background: []}],
+    [{script:"sub"}, {script: "super"}],
+    [{align: []}],
+    ["image" , "blackquote" , "code-block"],
+    ["clean"],
+]
+export default function TextEditor(){ 
+    //Set state to be able to connect to sockets
+    const [socket, setSocket] = useState()
+    const [quill, setQuill] = useState()
+    //Set State for the Toolbar
+    const [state, setState] = React.useState({ value: null });
+    const handleChange = value => {
+      setState({ value });
+    };
+
+    useEffect(() => {
+        const s = io("http://localhost:3001")  // connects to the server url that had been declared on server.js
+        setSocket(s)                           //initiate the socket to variable S
+
+        return() => {                          //disconnect from our server when no longer needed
+            s.disconnect()
+        }
+    }, [])
+
+    //Function to act as the receiving event from the server
+    useEffect(() => {
+        //Check if socket or quill appears or not, otherwise return
+        if( socket == null || quill == null) return
+
+        const handler = (delta) => {
+            quill.updateContents(delta)
+        }
+        socket.on("receive-changes", handler)
+        
+        return() => {
+            socket.off("receive-changes", handler)
+        }
+    }, [socket, quill])
+
+
+
+    //Function to detect changes on the documents using delta API from Quill
+    useEffect(() => {
+        //Check if socket or quill appears or not, otherwise return
+        if( socket == null || quill == null) return
+
+        const handler = (delta, oldDelta, source) => {
+            //If source changes not by user, return nothing
+            if (source !== 'user') return
+            //send the changes (delta) to the server
+            socket.emit("send-changes", delta) //delta denoted the subset of changes
+        }
+        quill.on('text-change', handler)
+        
+        return() => {
+            quill.off("text-change", handler)
+        }
+    }, [socket, quill])
+
+    const wrapperRef = useCallback(wrapper => {
+        if (wrapper == null) return
+        wrapper.innerHTML = ""
+        const editor = document.createElement('div')
+        wrapper.append(editor) //editor is put inside wrapperRef
+        const q = new Quill(editor, {
+            theme: "snow",
+            modules: { toolbar: TOOLBAR },
+        }) //quill is stated in editor 
+        setQuill(q)
+    }, [])
+
+
+        useEffect(() => {
+
+        return() =>{ 
+            wrapperRef.innerHTML = "" //clears everything out
+        }
+    }, [])                                 //useEffect is used here so the new instance of quill only run once
+    return (
+        <div className="container" ref={wrapperRef}></div>) //makes the header 
+} 
--- server/server.js
+++ server/server.js
@@ -1,14 +1,17 @@
+//Function to do the connections
 const io = require('socket.io')(3001, {     //sets 3001 to make it a different url 
     cors:   {                               //course origin request support to make the request from a diff url
-        origin: 'http://localhost:3000',   //uses the client port
-        methods: ['GET', 'POST'],  
+        origin: 'http://localhost:3000',   //local host as the origin of connection
+        methods: ['GET', 'POST'],          //Do a GET request and POST request to retrieve the connection
                 
     },
 })
-
+//If connections established notify through socket
 io.on("connection", socket => {
-    console.log("connected")
+    //Add event listener to receive changes from clients (delta)
+    socket.on('send-changes', delta => {
+        socket.broadcast.emit("receive-changes", delta)
+    })
 })
-
 
 var cors = require('cors')
